91851190fe5eb05547d2816e17f9bf4040e9b36a
lezzles11 <lesleyc@bu.edu>
2020-04-04 18:17:30 -0400

    pulling off of github

--- frontend/build/precache-manifest.a65e33622a5cfd65f3f7c4719f457fe3.js
+++ frontend/build/precache-manifest.a65e33622a5cfd65f3f7c4719f457fe3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bbe99f4e60c041e4aed1d997b72a7f29",
+    "revision": "d9b25632c380b6eaedc4d6b01af4c0fb",
     "url": "/index.html"
   },
   {
-    "revision": "770235dd3a27a02b297f",
+    "revision": "f6b08a12008f0233bf0a",
     "url": "/static/css/2.c51b6406.chunk.css"
   },
   {
-    "revision": "d41780e8a0df572e9775",
+    "revision": "a85fc436a37a740877f6",
     "url": "/static/css/main.d060fcbb.chunk.css"
   },
   {
-    "revision": "770235dd3a27a02b297f",
-    "url": "/static/js/2.f37d678f.chunk.js"
+    "revision": "f6b08a12008f0233bf0a",
+    "url": "/static/js/2.b4c6db89.chunk.js"
   },
   {
     "revision": "b46eae19120e0343c25f399c83a404d7",
-    "url": "/static/js/2.f37d678f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b4c6db89.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d41780e8a0df572e9775",
-    "url": "/static/js/main.96d1c780.chunk.js"
+    "revision": "a85fc436a37a740877f6",
+    "url": "/static/js/main.8f274460.chunk.js"
   },
   {
     "revision": "43d3749811e8bbf6b2ad",
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5928083964dde2fca7c6bab3d075adf.js"
+  "/precache-manifest.a65e33622a5cfd65f3f7c4719f457fe3.js"
 );
 
 self.addEventListener('message', (event) => {

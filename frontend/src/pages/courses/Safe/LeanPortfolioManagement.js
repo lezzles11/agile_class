23cc713d6b84789e23734b66dadfc357c8eb3a12
lezzles11 <lesleyc@bu.edu>
2020-04-07 23:19:29 -0400

    updated

--- frontend/src/pages/courses/Safe/LeanPortfolioManagement.js
+++ frontend/src/pages/courses/Safe/LeanPortfolioManagement.js
@@ -5,23 +5,22 @@ class LeanPortfolioManagement extends Component {
   render() {
     return (
       <div>
-        <Template 
-
-        logoimage="https://www.dropbox.com/s/g8ipley2fecp77n/LPM.png?raw=1" 
-        title="Certified SAFe® Lean Portfolio Manager" 
-        about="A foundational Lean-Agile and Portfolio operations course for SAFe portfolio managers." 
-        learn="A Certified SAFe Lean Portfolio Manager certificate holder will be expected to show competency in all aspects of managing an Agile iterative software development environment.  From the SAFe portfolio manager's perspective, the course prepares the student to demonstrate their expertise in managing an enterprise-wide portfolio of Agile projects. Instructors draw from their own real-life industry experience to show students key SAFe techniques for establishing a portfolio vision, for performing Lean portfolio budgeting, port and for using portfolio tools like Portfolio Kanban. Students are given an opportunity to practice and apply these same skills with scenarios drawn from actual industry problems. As this is a foundational level course, participants gain an appreciation both for the power, as well as the shortcomings, of Lean portfolio management in an enterprise setting.
+        <Template
+          logoimage="https://www.dropbox.com/s/g8ipley2fecp77n/LPM.png?raw=1"
+          title="Certified SAFe® Lean Portfolio Manager"
+          about="A foundational Lean-Agile and Portfolio operations course for SAFe portfolio managers."
+          learn="A Certified SAFe Lean Portfolio Manager certificate holder will be expected to show competency in all aspects of managing an Agile iterative software development environment.  From the SAFe portfolio manager's perspective, the course prepares the student to demonstrate their expertise in managing an enterprise-wide portfolio of Agile projects. Instructors draw from their own real-life industry experience to show students key SAFe techniques for establishing a portfolio vision, for performing Lean portfolio budgeting, port and for using portfolio tools like Portfolio Kanban. Students are given an opportunity to practice and apply these same skills with scenarios drawn from actual industry problems. As this is a foundational level course, participants gain an appreciation both for the power, as well as the shortcomings, of Lean portfolio management in an enterprise setting.
  
  
         After completing this three-day foundational course, participants will be able to demonstrate their knowledge of the theory and practice needed for implementing SAFe Lean portfolio management practices in their work.  All students who successfully complete the course will be prepared to take the Certified SAFe® Lean Portfolio Manager exam.
          
         Unlike any other training program, we offer the following bonuses:
-        " 
-        point1 = "The highest passing rates: The Certified SAFe® Lean Portfolio Manager certification exam is rigorous, but you will be prepared. Nearly 100% of our graduates pass the exam on their very first attempt. In the unlikely event that you would need to retake the exam, a second trial is free and additional examinations are discounted."
-        point2 = "Free follow-up access to your course materials for one year."
-        point3 = "25% discount on future course fees. Save on courses as you sail through the certification process."
-        point4 = "Anytime availability: Your course are available over the Internet 7 days a week and 24 hours a day. Study at any day and time you want. Once you are ready, simply sign up to take the certification exam!"
-        Prerequisites = "Some experience with Agile software management principles and practices will be helpful, whether from actual industry experience or from reading. "
+        "
+          point1="The highest passing rates: The Certified SAFe® Lean Portfolio Manager certification exam is rigorous, but you will be prepared. Nearly 100% of our graduates pass the exam on their very first attempt. In the unlikely event that you would need to retake the exam, a second trial is free and additional examinations are discounted."
+          point2="Free follow-up access to your course materials for one year."
+          point3="25% discount on future course fees. Save on courses as you sail through the certification process."
+          point4="Anytime availability: Your course are available over the Internet 7 days a week and 24 hours a day. Study at any day and time you want. Once you are ready, simply sign up to take the certification exam!"
+          Prerequisites="Some experience with Agile software management principles and practices will be helpful, whether from actual industry experience or from reading. "
         />
       </div>
     );
--- frontend/src/pages/courses/Safe/ReleaseTrainEngineer.js
+++ frontend/src/pages/courses/Safe/ReleaseTrainEngineer.js
@@ -5,24 +5,23 @@ class ReleaseTrainEngineer extends Component {
   render() {
     return (
       <div>
-        <Template 
-
-        logoimage="https://www.dropbox.com/s/aaj37uew3lcms5d/RTE.png?raw=1" 
-        title="Certified SAFe® Release Train Engineer" 
-        about="An intermediate- to advanced-level course on Agile Release Trains for Agile leaders, coaches and Scrum Masters." 
-        learn="This intermediate- to advanced-level three-day course provides attendees the knowledge to implement Agile Release Trains (ARTs) using the Scaled Agile Framework® (SAFe®) in a Lean-Agile enterprise.  The training is oriented towards Agile leaders, coaches, and Scrum Masters. Starting with a review of Lean-Agile principles, you will be guided through the intricacies of Agile Release Trains and the challenges for implementing these principles in an enterprise setting.
+        <Template
+          logoimage="https://www.dropbox.com/s/aaj37uew3lcms5d/RTE.png?raw=1"
+          title="Certified SAFe® Release Train Engineer"
+          about="An intermediate- to advanced-level course on Agile Release Trains for Agile leaders, coaches and Scrum Masters."
+          learn="This intermediate- to advanced-level three-day course provides attendees the knowledge to implement Agile Release Trains (ARTs) using the Scaled Agile Framework® (SAFe®) in a Lean-Agile enterprise.  The training is oriented towards Agile leaders, coaches, and Scrum Masters. Starting with a review of Lean-Agile principles, you will be guided through the intricacies of Agile Release Trains and the challenges for implementing these principles in an enterprise setting.
  
         A Certified SAFe Release Train Engineer certificate holder will be expected to have a thorough understanding of the key Lean-Agile concepts and practices. From the perspective of an already expert SAFe practitioner, the course prepares the student for advanced leadership, coaching and Scrum Master responsibilities in a enterprise SAFe environment.  Instructors draw from their own real-life experience to immerse the student in actual business scenarios.  From there, instructors guide students to create, manage and deploy ARTs. As this is intermediate- to advanced-level training, students gain an appreciation both for the power, as well as the shortcomings, of ARTs in an enterprise setting.
          
         After completing the course, participants will be able to demonstrate their knowledge of the theory and practice needed for implementing Agile Release Trains in a Lean-Agile environment.  All students who successfully complete the course will be prepared to take the Certified SAFe® Release Train Engineer  exam.
          
         Unlike any other training program, we offer the following bonuses:
-        " 
-        point1 = "The highest passing rates: The Certified SAFe® Release Train Engineer certification exam is rigorous, but you will be prepared. Nearly 100% of our graduates pass the exam on their very first attempt. In the unlikely event that you would need to retake the exam, a second trial is free and additional examinations are discounted."
-        point2 = "Free follow-up access to your course materials for one year."
-        point3 = "25% discount on future course fees. Save on courses as you sail through the certification process."
-        point4 = "Anytime availability: Your course are available over the Internet 7 days a week and 24 hours a day. Study at any day and time you want. Once you are ready, simply sign up to take the certification exam!"
-        Prerequisites =" Experience with Agile software development principles and practices is necessary, whether from actual industry experience or theory. "
+        "
+          point1="The highest passing rates: The Certified SAFe® Release Train Engineer certification exam is rigorous, but you will be prepared. Nearly 100% of our graduates pass the exam on their very first attempt. In the unlikely event that you would need to retake the exam, a second trial is free and additional examinations are discounted."
+          point2="Free follow-up access to your course materials for one year."
+          point3="25% discount on future course fees. Save on courses as you sail through the certification process."
+          point4="Anytime availability: Your course are available over the Internet 7 days a week and 24 hours a day. Study at any day and time you want. Once you are ready, simply sign up to take the certification exam!"
+          Prerequisites=" Experience with Agile software development principles and practices is necessary, whether from actual industry experience or theory. "
         />
       </div>
     );
